e6a910c1fc99595824db5c85655323f4aace8988
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2013-12-24 14:21:02 -0500

    updated blog to use Objectid in Categories model, refs f3-lib#24

--- src/Blog/Admin/Models/Categories.php
+++ src/Blog/Admin/Models/Categories.php
@@ -27,7 +27,7 @@ class Categories extends \Dsc\Models\Categories
         
         if ($doUpdate) 
         {
-            $id = (string) $update->id;
+            $id = $update->id;
             $title = $update->title;
             
             // update the category in the Posts collection
@@ -50,7 +50,7 @@ class Categories extends \Dsc\Models\Categories
     public function delete( $mapper, $options=array() )
     {
         // If a category is deleted, remove it from any nested 'categories' documents
-        $id = (string) $mapper->id;
+        $id = $mapper->id;
         
         if ($delete = parent::delete( $mapper, $options )) 
         {
