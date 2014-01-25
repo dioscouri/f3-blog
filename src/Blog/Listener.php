f299a906197a5410e1c9702f533e529485eb17b4
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-01-25 10:58:13 -0500

    prioritized admin nav, refs dioscouri/f3-admin#24

--- src/Blog/Listener.php
+++ src/Blog/Listener.php
@@ -8,6 +8,7 @@ class Listener extends \Prefab
         if ($mapper = $event->getArgument('mapper')) 
         {
             $mapper->reset();
+            $mapper->priority = 30;
             $mapper->id = 'f3-blog';
             $mapper->title = 'Blog';
             $mapper->route = '';
