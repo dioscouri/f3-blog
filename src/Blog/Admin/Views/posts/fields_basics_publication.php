0c8c3b1a4fa7f04664b9010b90a54c3d2426d336
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-02-27 00:02:32 -0500

    blog editing tweaks

--- src/Blog/Admin/Views/posts/fields_basics_publication.php
+++ src/Blog/Admin/Views/posts/fields_basics_publication.php
@@ -23,11 +23,16 @@
             <label>Start:</label>
             <div class="row">
                 <div class="col-md-6">
-                    <input name="publication[start_date]" value="<?php echo $flash->old('publication.start_date', date('Y-m-d') ); ?>" class="ui-datepicker form-control" type="text" data-date-format="yyyy-mm-dd" data-date-today-highlight="true" data-date-today-btn="true">
+                    <div class="input-group">
+                        <input name="publication[start_date]" value="<?php echo $flash->old('publication.start_date', date('Y-m-d') ); ?>" class="ui-datepicker form-control" type="text" data-date-format="yyyy-mm-dd" data-date-today-highlight="true" data-date-today-btn="true">
+                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                    </div>                    
                 </div>
-                <div class="input-group col-md-6">
-                    <input name="publication[start_time]" value="<?php echo $flash->old('publication.start_time' ); ?>" type="text" class="ui-timepicker form-control" data-show-meridian="false" data-show-inputs="false">
-                    <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
+                <div class="col-md-6">
+                    <div class="input-group">
+                        <input name="publication[start_time]" value="<?php echo $flash->old('publication.start_time' ); ?>" type="text" class="ui-timepicker form-control" data-show-meridian="false" data-show-inputs="false">
+                        <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -35,11 +40,16 @@
             <label>Finish:</label>
             <div class="row">
                 <div class="col-md-6">
-                    <input name="publication[end_date]" value="<?php echo $flash->old('publication.end_date' ); ?>" class="ui-datepicker form-control" type="text" data-date-format="yyyy-mm-dd" data-date-today-highlight="true" data-date-today-btn="true">
+                    <div class="input-group">
+                        <input name="publication[end_date]" value="<?php echo $flash->old('publication.end_date' ); ?>" class="ui-datepicker form-control" type="text" data-date-format="yyyy-mm-dd" data-date-today-highlight="true" data-date-today-btn="true">
+                        <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                    </div>                        
                 </div>
-                <div class="input-group col-md-6">
-                    <input name="publication[end_time]" value="<?php echo $flash->old('publication.end_time' ); ?>" type="text" class="ui-timepicker form-control" data-default-time="false" data-show-meridian="false" data-show-inputs="false">
-                    <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
+                <div class="col-md-6">
+                    <div class="input-group">
+                        <input name="publication[end_time]" value="<?php echo $flash->old('publication.end_time' ); ?>" type="text" class="ui-timepicker form-control" data-default-time="false" data-show-meridian="false" data-show-inputs="false">
+                        <span class="input-group-addon"><i class="fa fa-clock-o"></i></span>
+                    </div>
                 </div>
             </div>
             <span class="help-text">Leave these blank to never unpublish.</span>
