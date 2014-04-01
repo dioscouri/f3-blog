849d9d2fd6e8d1bd15efe4f5e9c7bdc0682557ee
Lukas Polak <lpolak@dioscouri.com>
2014-03-31 23:09:33 -0400

    Fixed settings model

--- src/Blog/Models/Settings.php
+++ src/Blog/Models/Settings.php
@@ -1,31 +1,11 @@
 <?php 
 namespace Blog\Models;
 
-class Settings extends \Dsc\Models\Settings 
+class Settings extends \Dsc\Mongo\Collections\Settings
 {
     public $home = array(
         'include_categories_widget' => 1
     );
     
-    protected $type = 'blog.settings';
-    
-    public function prefab( $source=array(), $options=array() )
-    {
-        $prefab = new \Blog\Models\Prefabs\Settings($source, $options);
-    
-        return $prefab;
-    }
-    
-    /**
-     * An alias for the save command, used only for creating a new object
-     *
-     * @param array $values
-     * @param array $options
-     */
-    public function create( $values, $options=array() )
-    {
-        $values = array_merge( $this->prefab()->cast(), $values );
-    
-        return $this->save( $values, $options );
-    }
+    public $type = 'blog.settings';
 }
