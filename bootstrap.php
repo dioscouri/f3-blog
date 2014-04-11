0c3f6ee39e27f267324b92fee45f47233b9e60fc
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-04-10 21:15:50 -0400

    Minor formatting changes

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,7 @@
-<?php 
-class BlogBootstrap extends \Dsc\Bootstrap{
-	protected $dir = __DIR__;
-	protected $namespace = 'Blog';
+<?php
+class BlogBootstrap extends \Dsc\Bootstrap
+{
+    protected $dir = __DIR__;
+    protected $namespace = 'Blog';
 }
 $app = new BlogBootstrap();
--- src/Blog/Admin/Routes.php
+++ src/Blog/Admin/Routes.php
@@ -1,37 +1,34 @@
 <?php
-
 namespace Blog\Admin;
 
 /**
  * Group class is used to keep track of a group of routes with similar aspects (the same controller, the same f3-app and etc)
  */
-class Routes extends \Dsc\Routes\Group{
-	
-	
-	function __construct(){
-		parent::__construct();
-	}
-	
-	/**
-	 * Initializes all routes for this group
-	 * NOTE: This method should be overriden by every group
-	 */
-	public function initialize(){
-		$this->setDefaults(
-				array(
-					'namespace' => '\Blog\Admin\Controllers',
-					'url_prefix' => '/admin/blog'
-				)
-		);
-
-		$this->addSettingsRoutes();
-		$this->addCrudGroup( 'Posts', 'Post');
-		$this->addCrudGroup( 'Categories', 'Category', array( 'datatable_links' => true, 'get_parent_link' => true ));
-		$this->add( '/checkboxes', array( 'GET', 'POST' ),
-					array(
-						'controller' => 'Categories',
-						'action' => 'getCheckboxes'
-						)
-					);
-	}
+class Routes extends \Dsc\Routes\Group
+{
+    /**
+     * Initializes all routes for this group
+     * NOTE: This method should be overriden by every group
+     */
+    public function initialize()
+    {
+        $this->setDefaults( array(
+            'namespace' => '\Blog\Admin\Controllers',
+            'url_prefix' => '/admin/blog' 
+        ) );
+        
+        $this->addSettingsRoutes();
+        $this->addCrudGroup( 'Posts', 'Post' );
+        $this->addCrudGroup( 'Categories', 'Category', array(
+            'datatable_links' => true,
+            'get_parent_link' => true 
+        ) );
+        $this->add( '/checkboxes', array(
+            'GET',
+            'POST' 
+        ), array(
+            'controller' => 'Categories',
+            'action' => 'getCheckboxes' 
+        ) );
+    }
 }
