20be9b05143cca31831321d59fa1aa66ab947a33
Lukas Polak <lpolak@dioscouri.com>
2014-04-21 00:05:17 -0400

    added comment count

--- src/Blog/Site/Views/posts/view_comments.php
+++ src/Blog/Site/Views/posts/view_comments.php
@@ -1,11 +1,28 @@
 <?php
 	$settings = \Blog\Models\Settings::fetch()->populateState()->getItem();
 	if( ( $type = $settings->get( "general.comments" ) ) != 'null' ) {
-		?>
+
+		
+	// get number of comments
+	$num_comments = '5';
+	$post_url =  \Base::instance()->get('SCHEME').'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
+	switch( $type ) {
+		case 'facebook' :
+			{
+				$num_comments = '<div class="fb-comments-count" data-href="'.$post_url.'">0</div>';
+				break;
+			}
+		case 'disqus':
+			{
+				$num_comments = '<a href="'.$post_url.'#disqus_thread" data-disqus-identifier="'.$item->{'id'}.'">First article</a>';
+				break;
+			}
+	}
+?>
 
 <div class="blog-comments main-widget">
 	<div class="widget-title">
-		<h2>Comments (4)</h2>
+		<h2>Comments (<?php echo $num_comments?>)</h2>
 	</div>
 	<div class="widget-content">
 
@@ -16,7 +33,6 @@
 				$num_posts = $settings->get( 'social.facebook.num_posts' );
 				$theme = $settings->get( 'social.facebook.theme' );
 				$order = $settings->get( 'social.facebook.order' );
-				$url =  \Base::instance()->get('SCHEME').'://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'];
 			?>
 				<div id="fb-root"></div>
 				<script>(function(d, s, id) {
@@ -27,23 +43,31 @@
 				  fjs.parentNode.insertBefore(js, fjs);
 				}(document, 'script', 'facebook-jssdk'));</script>
 			
-				<div class="fb-comments" data-href="<?php echo $url;?>" data-numposts="<?php echo $num_posts?>" data-colorscheme="<?php echo $theme;?>" data-order-by="<?php echo $order;?>" data-mobile="<?php echo $mobile;?>"></div>
+				<div class="fb-comments" data-href="<?php echo $post_url;?>" data-numposts="<?php echo $num_posts?>" data-colorscheme="<?php echo $theme;?>" data-order-by="<?php echo $order;?>" data-mobile="<?php echo $mobile;?>"></div>
 			<?php
 				break;
 				
 			case 'disqus':
 				$shortname = $settings->get( 'social.disqus.shortname' );
+				$title = $item->{'title'};
+				$identifier = $item->{'id'};
+				
 				?>
 				<div id="disqus_thread"></div>
 			    <script type="text/javascript">
-			        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
 			        var disqus_shortname = '<?php echo $shortname; ?>'; // required: replace example with your forum shortname
-			
-			        /* * * DON'T EDIT BELOW THIS LINE * * */
+			        var disqus_identifier = '<?php echo $identifier;?>';
+			        var disqus_title = '<?php echo $title;?>';
+			        			
 			        (function() {
 			            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
 			            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
 			            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+
+			            var c = document.createElement('script'); c.async = true;
+			            c.type = 'text/javascript';
+			            c.src = '//' + disqus_shortname + '.disqus.com/count.js';
+			            (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(c);
 			        })();
 			    </script>
 			    <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
