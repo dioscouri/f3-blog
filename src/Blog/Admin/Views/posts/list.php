c62ae5369391d0a2b246da566a160b976ff403ad
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-05-22 20:47:15 -0400

    missing closing tag

--- src/Blog/Admin/Views/posts/list.php
+++ src/Blog/Admin/Views/posts/list.php
@@ -1,8 +1,8 @@
 <script type="text/javascript">
 jQuery(function(jQuery) { 
-	jQuery("select[data-select]").on('change', function(){
-		form.submit();
-		}).select2();
+	jQuery("select[data-select]").select2().on('change', function(){
+		jQuery(this).closest('form').submit();
+	});
 });
 
 </script>
@@ -32,21 +32,22 @@ jQuery(function(jQuery) {
                 <ul class="list-filters list-unstyled list-inline">
                     <li class="col-md-3">
 						<select name="filter[category][slug]" data-select='1'>
-<?php 
-	echo \Dsc\Html\Select::options($categories, $state->get('filter.category.slug'));
-?>
+                            <?php 
+                            	echo \Dsc\Html\Select::options($categories, $state->get('filter.category.slug'));
+                            ?>
 						</select>
                     </li>
                     <li class="col-md-3">
 						<select name="filter[tags]" data-select='1'>
-<?php 
-	echo \Dsc\Html\Select::options($all_tags, $state->get('filter.tags'));
-?>                    			
+                            <?php 
+                            	echo \Dsc\Html\Select::options($all_tags, $state->get('filter.tags'));
+                            ?>
 						</select>
                     </li>
                 </ul>    
                         
             </div>
+            
             <div class="col-xs-12 col-sm-7 col-md-7 col-lg-4">
                 <div class="form-group">
                     <div class="input-group">
@@ -123,71 +124,69 @@ jQuery(function(jQuery) {
         <input type="hidden" name="list[direction]" value="<?php echo $state->get('list.direction'); ?>" />
 
         <?php if (!empty($paginated->items)) { ?>
-        <?php foreach($paginated->items as $blog) {
-        	$featured_img = $blog->{'featured_image.slug'};
-        	$has_featured_img = !empty( $featured_img );
-        	?>
+        <?php foreach($paginated->items as $item) { ?>
             <div class="panel panel-default">
                 <div class="panel-body">
                     <div class="row">
                         <div class="checkbox-column col-xs-1 col-sm-1 col-md-1">
-							<input type="checkbox" class="icheck-input" name="ids[]" value="<?php echo $blog->id; ?>">
+							<input type="checkbox" class="icheck-input" name="ids[]" value="<?php echo $item->id; ?>">
                         </div>
                     
                         <div class="col-xs-11 col-sm-11 col-md-11">
                         	<div class="row">
-                        		<div class="hidden-xs hidden-sm col-md-4 col-lg-4">
-								<?php if( $has_featured_img ) { ?>
-                        			<a href="./admin/blog/post/edit/<?php echo $blog->id; ?>" title="<?php echo $blog->title; ?>">
-                        			<img src="./asset/thumb/<?php echo $featured_img; ?>" style="max-width : 100%"  alt="<?php echo $blog->title; ?>" />
+                        		<div class="hidden-xs hidden-sm col-md-3 col-lg-3">
+								<?php if( !empty( $item->{'featured_image.slug'} ) ) { ?>
+                        			<a href="./admin/blog/post/edit/<?php echo $item->id; ?>" title="<?php echo $item->title; ?>">
+                        			<img class="img-responsive" src="./asset/thumb/<?php echo $item->{'featured_image.slug'}; ?>" alt="<?php echo $item->title; ?>" />
                         			</a>
                         		<?php } else { ?>
                         			&nbsp;
                         		<?php } ?>
                         		</div>
                         		
-                        		<div class="col-xs-12 col-sm-12 col-md-7 col-lg-7">
+                        		<div class="col-xs-12 col-sm-12 col-md-8 col-lg-8">
                         			<div class="row">
 		                        		<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                        					<legend><a href="./admin/blog/post/edit/<?php echo $blog->id; ?>"><?php echo $blog->title; ?></a></legend>
+                        					<legend><a href="./admin/blog/post/edit/<?php echo $item->id; ?>"><?php echo $item->title; ?></a></legend>
 		                        		</div>
                         			</div>
                         			<div class="row">
 		                        		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 		                        			<div><h5>Basic</h5></div>
 		                        			<div>
-		                        				<label>/<?php echo $blog->slug; ?></label>
+		                        				<label>/<?php echo $item->slug; ?></label>
 		                        			</div>
 		                        			<div>
-		                        				<label>Author:</label> <a href="./admin/user/edit/<?php echo $blog->{'author.id'}?>" title="<?php echo $blog->{'author.name'}; ?>"><?php echo $blog->{'author.name'}; ?></a>
+		                        				<label>Author:</label> <a href="./admin/user/edit/<?php echo $item->{'author.id'}?>" title="<?php echo $item->{'author.name'}; ?>"><?php echo $item->{'author.name'}; ?></a>
 		                        			</div>
 		                        			<div>
-			                        			<?php $categories = \Joomla\Utilities\ArrayHelper::getColumn( (array) $blog->categories, 'title' ); ?>
+			                        			<?php $categories = \Joomla\Utilities\ArrayHelper::getColumn( (array) $item->categories, 'title' ); ?>
 		                        				<label>Categories:</label>
 												<span class='label label-warning'><?php echo implode("</span> <span class='label label-warning'>", (array) $categories ); ?></span>
 		                        			</div>
 		                        			<div>
 		                        				<label>Tags:</label>
-												<span class='label label-success'><?php echo implode("</span> <span class='label label-success'>", (array) $blog->tags); ?></span>
+												<span class='label label-success'><?php echo implode("</span> <span class='label label-success'>", (array) $item->tags); ?></span>
 		                        			</div>
 		                        		</div>
 		                        		
 		                        		<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
-		                        			<div><h5>Publication</h5></div>
+		                        			<h5>Publication</h5>
 				                            <div>
-				                            	<label>Status:</label> <?php echo ucwords( $blog->{'publication.status'} ); ?>
+				                            	<label>Status:</label> <?php echo ucwords( $item->{'publication.status'} ); ?>
 				                            </div>
-				                            <?php if ($blog->{'publication.start_date'}) { ?>
+				                            <?php if ($item->{'publication.start_date'}) { ?>
 				                            <div>
-				                            	<label>Up:</label> <?php echo $blog->{'publication.start_date'}; ?>
+				                            	<label>Up:</label> <?php echo $item->{'publication.start_date'}; ?>
 				                            </div>
 				                            <?php } ?>
-				                            <?php if ($blog->{'publication.end_date'}) { ?>
+				                            <?php if ($item->{'publication.end_date'}) { ?>
 				                            <div>
-				                            	<label>Down:</label> <?php echo $blog->{'publication.end_date'}; ?>
+				                            	<label>Down:</label> <?php echo $item->{'publication.end_date'}; ?>
+				                            </div>
 				                            <?php } ?>
 				                            <div>
-				                            	<label>By</label> <a href="./admin/user/edit/<?php echo $blog->{'author.id'}; ?>" alt="<?php echo $blog->{'author.name'}; ?>" ><?php echo $blog->{'author.name'}; ?></a>
+				                            	<label>By</label> <a href="./admin/user/edit/<?php echo $item->{'author.id'}; ?>" alt="<?php echo $item->{'author.name'}; ?>" ><?php echo $item->{'author.name'}; ?></a>
 				                            </div>
 		                        		</div>
 		                        		
@@ -195,7 +194,7 @@ jQuery(function(jQuery) {
                         		</div>
 
 		                        <div class="hidden-xs hidden-sm col-md-1 col-lg-1">
-			                        <a class="btn btn-xs btn-danger" data-bootbox="confirm" href="./admin/blog/post/delete/<?php echo $blog->id; ?>">
+			                        <a class="btn btn-xs btn-danger" data-bootbox="confirm" href="./admin/blog/post/delete/<?php echo $item->id; ?>">
 			                            <i class="fa fa-times"></i>
 			                        </a>
 		                    	</div>
