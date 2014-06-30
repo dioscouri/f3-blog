b052c29a1882d170fe405a7a8a290da0a7eb6239
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-06-30 09:31:16 -0400

    curated priority of search scopes

--- bootstrap.php
+++ bootstrap.php
@@ -1,39 +1,47 @@
 <?php
 class BlogBootstrap extends \Dsc\Bootstrap
 {
+
     protected $dir = __DIR__;
+
     protected $namespace = 'Blog';
-    
+
     protected function preSite()
     {
         if (class_exists('\Search\Factory'))
         {
-            \Search\Factory::registerSource( new \Search\Models\Source( array(
-                'id'=>'blog.posts', 'title'=>'Blog Posts', 'class'=>'\Blog\Models\Posts'
-            ) ) );            
+            \Search\Factory::registerSource(new \Search\Models\Source(array(
+                'id' => 'blog.posts',
+                'title' => 'Blog Posts',
+                'class' => '\Blog\Models\Posts'
+            )));
         }
-                
+        
         if (class_exists('\Modules\Factory'))
         {
-            \Modules\Factory::registerPositions( array( 'blog-tag-cloud') );
+            \Modules\Factory::registerPositions(array(
+                'blog-tag-cloud'
+            ));
         }
-    	    	
     }
-    
+
     protected function preAdmin()
     {
         if (class_exists('\Modules\Factory'))
         {
-            \Modules\Factory::registerPositions( array( 'blog-tag-cloud') );
-        }            	
-
-    	if (class_exists('\Search\Factory'))
-    	{
-    	    \Search\Factory::registerSource( new \Search\Models\Source( array(
-    	        'id'=>'blog.posts', 'title'=>'Blog Posts', 'class'=>'\Blog\Models\Posts'
-    	    ) ) );
-    	    	
-    	}    	
+            \Modules\Factory::registerPositions(array(
+                'blog-tag-cloud'
+            ));
+        }
+        
+        if (class_exists('\Search\Factory'))
+        {
+            \Search\Factory::registerSource(new \Search\Models\Source(array(
+                'id' => 'blog.posts',
+                'title' => 'Blog Posts',
+                'class' => '\Blog\Models\Posts'
+            )));
+        }
     }
 }
 $app = new BlogBootstrap();
