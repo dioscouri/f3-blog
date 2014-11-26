43341ec8ee3565ffaeea4a5f7dcc9e07796aee6a
Building Berms Server <chris@ammonitenetworks.com>
2014-11-26 15:18:54 +0000

    updating blogs post module

--- src/Blog/Modules/Posts/Views/default.php
+++ src/Blog/Modules/Posts/Views/default.php
@@ -1,36 +1,10 @@
-<div class="widget widget-category">
-	<div class="widget-title">
-		<h2>Categories</h2>
-	</div>
-	<div class="widget-content">
-		<div class="accordion">
-			<div class="panel-group">
-				<div class="panel-body">
-					<ul>
-			<?php
-			
-			$posts = (new \Blog\Modules\Posts)->setParam('limit',5)->getList();?>
-		
-				<li>
-					<div class="panel panel-default">
-						<div class="panel-heading">
-							<h4 class="panel-title">
-								<a href="#">
-									No Categories
-								</a>
-							</h4>
-						</div>
-						<div id="collapseTwo" class="panel-collapse collapse">
-							<div class="panel-body">
-							</div>
-						</div>
-					</div>
-				</li>
-
-				
-					</ul>
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<h4 class="">Newest Posts</h4>
+							<ul>
+							<?php $posts = (new \Blog\Models\Posts)->setState('filter.publication_status', 'published')
+                ->setState('filter.published_today', true)->setParam('limit',5)->getList(); ?>
+							
+							<?php foreach($posts as $post) : ?>
+								<li><a href="/blog/post/<?php echo  $post->slug; ?>"><?php echo  $post->title; ?></a></li>
+								<?php endforeach;?>
+								
+							</ul>
